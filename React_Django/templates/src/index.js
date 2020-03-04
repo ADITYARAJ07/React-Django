c293524871bef4a6050679f05085b5a426da80ef
ADITYARAJ07 <adityarajvarma26@gmail.com>
2020-03-04 17:53:34 +0530

    first

---
+++ React_Django/templates/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Hello Aditya</h1>;
+console.log(element);
+ReactDOM.render(element, document.getElementById("root"));
